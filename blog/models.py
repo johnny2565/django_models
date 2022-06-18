256a4c31888dd43d409cb9a59f8d44fef44f8076
Johnny2565 <masterj6525@gmail.com>
2022-06-18 07:51:30 +0100

    successfully made some migrations

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from pyexpat import model
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+# Create your models here.
+
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TimeField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
